36acff52ed03d1ec91ddcb5843a1119788145bd3
zhengqiang.tan <844870676@qq.com>
2021-07-05 00:14:54 +0800

    flink scala 开发wordcount

--- src/main/scala/com/test/FistApp.scala
+++ src/main/scala/com/test/FistApp.scala
@@ -1,8 +1,30 @@
 package com.test
 
+import org.apache.flink.api.scala.ExecutionEnvironment
+import org.apache.flink.api.scala._
+
 object FistApp {
   def main(args: Array[String]): Unit = {
-    val str = "world"
-    println(s"hello $str")
+
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    val text = env.readTextFile("/Users/tandemac/workspace/lagou/TanFlink/data/wordcount.txt")
+
+    //    val counts = text
+    //      .flatMap(_.toLowerCase().split("\\W+"))
+    //      .filter(_.nonEmpty)
+    //      .map((_, 1))
+    //      .groupBy(0)
+    //      .sum(1)
+
+
+    // 分词、转换、求和
+    val ws = text.flatMap(_.split(" ")).map((_, 1)).groupBy(0).sum(1)
+    ws.print()
+
+
+    //    env.execute("wordcount")
+
+
   }
 }
